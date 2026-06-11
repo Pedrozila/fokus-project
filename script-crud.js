489df058b848136826b53ff65dbd5b6b319696c1
Pedro Bruno <pedrobn97@outlook.com>
2026-06-11 13:49:12 -0300

    feat: implement task addition functionality with form submission handling

--- script-crud.js
+++ script-crud.js
@@ -1,6 +1,18 @@
 const btnAdiciona = document.querySelector('.app__button--add-task')
 const form = document.querySelector('.app__form-add-task')
+const textArea = document.querySelector('.app__form-textarea')
+
+const tarefas = []
 
 btnAdiciona.addEventListener('click', () => {
     form.classList.toggle('hidden')
+})
+
+form.addEventListener('submit', (evento) => {
+    evento.preventDefault();
+    const tarefa = {
+        descricao: textArea.value
+    }
+    tarefas.push(tarefa)
+    localStorage.setItem('tarefas', JSON.stringify(tarefas))
 })
