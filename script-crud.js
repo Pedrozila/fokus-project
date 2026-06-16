7491a2963e1008cf07cb7c24233a6526e134ce28
Pedro Bruno <pedrobn97@outlook.com>
2026-06-16 14:10:56 -0300

    fix: correct task rendering logic and ensure tasks are displayed on load

--- script-crud.js
+++ script-crud.js
@@ -1,8 +1,9 @@
 const btnAdiciona = document.querySelector(".app__button--add-task");
 const form = document.querySelector(".app__form-add-task");
 const textArea = document.querySelector(".app__form-textarea");
+const ulTarefas = document.querySelector('.app__section-task-list')
 
-const tarefas = JSON.parse(localStorage.getItem('tarefas'));
+const tarefas = JSON.parse(localStorage.getItem('tarefas')) || []
 
 function criarElementoTarefa(tarefa) {
   const li = document.createElement("li");
@@ -30,6 +31,8 @@ function criarElementoTarefa(tarefa) {
     li.append(svg)
     li.append(paragrafo)
     li.append(botao)
+
+    return li
 }
 
 btnAdiciona.addEventListener("click", () => {
@@ -44,3 +47,8 @@ form.addEventListener("submit", (evento) => {
   tarefas.push(tarefa);
   localStorage.setItem("tarefas", JSON.stringify(tarefas));
 });
+
+tarefas.forEach(tarefa => {
+    const elementoTarefas = criarElementoTarefa(tarefa) 
+    ulTarefas.append('elementoTarefas')
+})
