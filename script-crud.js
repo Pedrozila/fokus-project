3d44ba100c0fdfcdee22cb8ff9ba08bf62806b1e
Pedro Bruno <pedrobn97@outlook.com>
2026-06-17 17:31:42 -0300

    fix: improve task editing functionality and enhance form handling

--- script-crud.js
+++ script-crud.js
@@ -1,13 +1,23 @@
 const btnAdiciona = document.querySelector(".app__button--add-task");
 const form = document.querySelector(".app__form-add-task");
 const textArea = document.querySelector(".app__form-textarea");
-const ulTarefas = document.querySelector('.app__section-task-list')
+const ulTarefas = document.querySelector(".app__section-task-list");
+const btnCancelar = document.querySelector('.app__form-footer__button--cancel')
 
-const tarefas = JSON.parse(localStorage.getItem('tarefas')) || []
+const tarefas = JSON.parse(localStorage.getItem("tarefas")) || [];
+const limparForm = () => {
+  textArea.value = '';
+  form.classList.add('hidden')
+}
+
 
 function atualizarTarefa() {
   localStorage.setItem("tarefas", JSON.stringify(tarefas));
 }
+
+btnCancelar.addEventListener('click', limparForm);
+
+
 
 function criarElementoTarefa(tarefa) {
   const li = document.createElement("li");
@@ -18,33 +28,36 @@ function criarElementoTarefa(tarefa) {
             <circle cx="12" cy="12" r="12" fill="#FFF"></circle>
             <path d="M9 16.1719L19.5938 5.57812L21 6.98438L9 18.9844L3.42188 13.4062L4.82812 12L9 16.1719Z" fill="#01080E"></path>
         </svg>
-    `
+    `;
 
-    const paragrafo = document.createElement('p')
-    paragrafo.textContent = tarefa.descricao
-    paragrafo.classList.add('app__section-task-list-item-description')
+  const paragrafo = document.createElement("p");
+  paragrafo.textContent = tarefa.descricao;
+  paragrafo.classList.add("app__section-task-list-item-description");
 
-    const botao = document.createElement('button')
-    botao.classList.add('app_button-edit')
+  const botao = document.createElement("button");
+  botao.classList.add("app_button-edit");
 
-    botao.onclick = () => {
-      const novaDescricao = prompt('Qual é o novo nome da tarefa?')
-      paragrafo.textContent = novaDescricao
-      tarefa.descricao = novaDescricao
-      atualizarTarefa()
+  botao.onclick = () => {
+    debugger
+    const novaDescricao = prompt("Qual é o novo nome da tarefa?");
+    if (novaDescricao) {
+      paragrafo.textContent = novaDescricao;
+      tarefa.descricao = novaDescricao;
+      atualizarTarefa();
     }
+  };
 
-    const imageButton = document.createElement('img')
+  const imageButton = document.createElement("img");
 
-    imageButton.setAttribute('src', './imagens/edit.png')
+  imageButton.setAttribute("src", "./imagens/edit.png");
 
-    botao.append(imageButton)
+  botao.append(imageButton);
 
-    li.append(svg)
-    li.append(paragrafo)
-    li.append(botao)
+  li.append(svg);
+  li.append(paragrafo);
+  li.append(botao);
 
-    return li
+  return li;
 }
 
 btnAdiciona.addEventListener("click", () => {
@@ -57,15 +70,14 @@ form.addEventListener("submit", (evento) => {
     descricao: textArea.value,
   };
   tarefas.push(tarefa);
-  const elementoTarefa = criarElementoTarefa(tarefa)
-  ulTarefas.append(elementoTarefa)
-  atualizarTarefa()
-  textArea.value = ''
-  form.classList.add('hidden')
-
+  const elementoTarefa = criarElementoTarefa(tarefa);
+  ulTarefas.append(elementoTarefa);
+  atualizarTarefa();
+  textArea.value = "";
+  form.classList.add("hidden");
 });
 
-tarefas.forEach(tarefa => {
-    const elementoTarefas = criarElementoTarefa(tarefa) 
-    ulTarefas.append(elementoTarefas)
-})
+tarefas.forEach((tarefa) => {
+  const elementoTarefas = criarElementoTarefa(tarefa);
+  ulTarefas.append(elementoTarefas);
+});
